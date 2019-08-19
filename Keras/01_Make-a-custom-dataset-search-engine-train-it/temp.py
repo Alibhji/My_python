f3cd029a19f1f1dd79ebbdb52d6a6ea3ac4df775
Ali Babolhavaeji <40876922+Alibhji@users.noreply.github.com>
2019-08-19 12:49:22 -0400

    Update temp.py

--- Keras/01_Make-a-custom-dataset-search-engine-train-it/temp.py
+++ Keras/01_Make-a-custom-dataset-search-engine-train-it/temp.py
@@ -17,14 +17,32 @@ import random
 import pickle
 import cv2
 import os
+import time
 
-
-args={"dataset":'images' , 
+IMAGE_DIMS = (96, 96, 3)
+args={"dataset":"..\\..\\..\\dataset_1" , 
       "model":'pokedex2.model',
        'labelbin':'lb2.pickle' }
 
+data=[]
+labels=[]
+
 # load the image
 imagePaths = sorted(list(paths.list_images(args["dataset"])))
-print(imagePaths)
+# print(list(paths.list_images(args["dataset"])))
 
 
+for imagePath in enumerate(imagePaths):
+      start_time = time.time()
+      image=cv2.imread(imagePath[1])
+      image.resize(IMAGE_DIMS[0],IMAGE_DIMS[1],IMAGE_DIMS[2])
+      image = img_to_array(image)
+      data.append(image)
+      # print(imagePaths[1])
+      label = imagePath[1].split(os.path.sep)[-2]
+      labels.append(label)
+      end_time= time.time()
+      print("Image {} is added. time: {}".format(imagePath[0],start_time-end_time))
+      
+      
+      
